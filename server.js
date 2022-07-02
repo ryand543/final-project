8f2c400a6fa181d0ab76f856ba6f8e4a0aea0a8e
ryand19 <ryand19@vt.edu>
2022-07-01 20:53:09 -0400

    trying to get homeworks to work

--- server.js
+++ server.js
@@ -4,21 +4,12 @@ const port = process.env.PORT || 5000
 
 const app = express()
 const publicPath = path.join(__dirname, 'build')
-const nasaPath = path.join(__dirname, 'nasa')
-const gridPath = path.join(__dirname, 'gridFormat')
 const p5Path = path.join(__dirname, 'p5')
 
-app.use("/projects/1", express.static(nasaPath))
-// app.use("/projects/2", express.static(gridPath))
 app.use("/projects/3", express.static(p5Path))
 app.use(express.static(publicPath))
 
-app.get("/projects/1", (req, res) => {
-    res.sendFile(path.join(gridPath, "nasa.html"))
-})
-// app.get("/projects/2", (req, res) => {
-//     res.sendFile(path.join(gridPath, ".html"))
-// })
+
 app.get("/projects/3", (req, res) => {
     res.sendFile(path.join(p5Path, "p5.html"))
 })
